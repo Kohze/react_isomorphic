1dc12582580c70f2eca3d6970cfc057c1e4c294f
Kohze <mlw.kohze@student.ru.nl>
2017-03-10 22:23:41 +0100

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Header.css';
+import Link from '../Link';
+import Navigation from '../Navigation';
+import logoUrl from './logo-small.png';
+import logoUrl2x from './logo-small@2x.png';
+import * as ReactDOM from 'react-dom';
+import { Button } from 'office-ui-fabric-react/lib/Button';
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className={s.root}>
+        <div className={s.container}>
+          <Navigation />
+          <Link className={s.brand} to="/">
+            <img src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="38" height="38" alt="React" />
+            <span className={s.brandTxt}>pRoloc</span>
+          </Link>
+          <div><Button>I am a button.</Button></div>
+          <div className={s.banner}>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(s)(Header);
